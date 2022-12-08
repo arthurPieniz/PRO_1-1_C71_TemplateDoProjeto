c47823535eceb8d35e690576d14a9626217322fa
arthurPieniz <102185959+arthurPieniz@users.noreply.github.com>
2022-12-08 15:48:55 -0300

    Colocando configuração firebase

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6uUpem4XHSL6ktwzP8nbXPbtVxZwLQfU",
+    authDomain: "pro-c71-81a88.firebaseapp.com",
+    projectId: "pro-c71-81a88",
+    storageBucket: "pro-c71-81a88.appspot.com",
+    messagingSenderId: "141664969121",
+    appId: "1:141664969121:web:052f8956549def92382be0",
+    measurementId: "G-F87G6R0DPV"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
